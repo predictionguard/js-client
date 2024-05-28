566900cfedb113ffcde5516121c21f9417db040a
William Kennedy <bill@predictionguard.com>
2024-05-28 19:36:48 -0400

    Version 0.8.0 with docs (#6)

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
     ...tseslint.configs.strict,
     {
-        ignores: ['dist/*'],
+        ignores: ['dist/*', 'docs/*'],
     },
     {
         rules: {
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,9 @@
-import client from './api_client.js';
-import chat from './api_chat.js';
-import completion from './api_completion.js';
-import factuality from './api_factuality.js';
-import health from './api_health.js';
-import injection from './api_injection.js';
-import replacepi from './api_replacepi.js';
-import toxicity from './api_toxicity.js';
-import translate from './api_translate.js';
-
-export {client};
-export {chat};
-export {completion};
-export {factuality};
-export {health};
-export {injection};
-export {replacepi};
-export {toxicity};
-export {translate};
+export * from './api_client.js';
+export * from './api_chat.js';
+export * from './api_completion.js';
+export * from './api_factuality.js';
+export * from './api_health.js';
+export * from './api_injection.js';
+export * from './api_replacepi.js';
+export * from './api_toxicity.js';
+export * from './api_translate.js';
